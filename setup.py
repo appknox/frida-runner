7058504bfe6a8aec41d6460bb2eb014615f74e26
dhilipsiva <dhilipsiva@gmail.com>
2016-06-30 13:32:56 +0530

    Use standard six

--- setup.py
+++ setup.py
@@ -18,13 +18,14 @@ setup(
     name='frida-runner',
     description="A Stupid CLI script to run Frida on iOS or Android",
     url='https://github.com/appknox/frida-runner',
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['frida_runner'],
     author='dhilipsiva',
     author_email='dhilipsiva@gmail.com',
     install_requires=[
         'Click',
-        'frida'
+        'frida',
+        'six==1.4.1',
     ],
     entry_points='''
         [console_scripts]
