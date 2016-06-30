25571cdaff5aef276f8090e0771e38070f6bd5df
dhilipsiva <dhilipsiva@gmail.com>
2016-06-30 13:46:27 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -18,14 +18,13 @@ setup(
     name='frida-runner',
     description="A Stupid CLI script to run Frida on iOS or Android",
     url='https://github.com/appknox/frida-runner',
-    version='0.0.3',
+    version='0.0.4',
     py_modules=['frida_runner'],
     author='dhilipsiva',
     author_email='dhilipsiva@gmail.com',
     install_requires=[
         'Click',
         'frida',
-        'six==1.4.1',
     ],
     entry_points='''
         [console_scripts]
