d7481a717acfb224af28c0eeb00546d00992c1df
dhilipsiva <dhilipsiva@gmail.com>
2016-07-04 10:08:13 +0530

    Spaces. No Tabs.

--- sample.js
+++ sample.js
@@ -1,10 +1,10 @@
 Interceptor.attach(Module.findExportByName('Foundation', 'NSLog'), {
-	onEnter: function(args) {
-		var text = new ObjC.Object(args[0]);
-		var vars = new ObjC.Object(args[1]);
-		text = text.toString();
-		vars = vars.toString();
-		var data = {'text': text, "vars": vars};
-		sendData(data);
-	}
+  onEnter: function(args) {
+    var text = new ObjC.Object(args[0]);
+    var vars = new ObjC.Object(args[1]);
+    text = text.toString();
+    vars = vars.toString();
+    var data = {'text': text, "vars": vars};
+    sendData(data);
+  }
 });
